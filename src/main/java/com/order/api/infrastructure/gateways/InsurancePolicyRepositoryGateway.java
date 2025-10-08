827fb8ea4187b8399bddd27ef5069c247405a456
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 20:38:20 -0300

    feat: implement InsurancePolicyRepositoryGateway for policy creation

---
+++ src/main/java/com/order/api/infrastructure/gateways/InsurancePolicyRepositoryGateway.java
@@ -0,0 +1,22 @@
+package com.order.api.infrastructure.gateways;
+
+import com.order.api.application.gateways.InsurancePolicyGateway;
+import com.order.api.domain.entity.InsurancePolicy;
+import com.order.api.infrastructure.persistence.InsurancePolicyEntity;
+import com.order.api.infrastructure.persistence.InsurancePolicyRepository;
+
+public class InsurancePolicyRepositoryGateway implements InsurancePolicyGateway {
+    private final InsurancePolicyMapper insurancePolicyMapper;
+    private final InsurancePolicyRepository insurancePolicyRepository;
+
+    public InsurancePolicyRepositoryGateway(InsurancePolicyMapper insurancePolicyMapper, InsurancePolicyRepository insurancePolicyRepository) {
+        this.insurancePolicyMapper = insurancePolicyMapper;
+        this.insurancePolicyRepository = insurancePolicyRepository;
+    }
+
+    public InsurancePolicy createInsurancePolicy(InsurancePolicy insurancePolicyDomainObj) {
+        InsurancePolicyEntity insurancePolicyEntity = insurancePolicyMapper.toEntity(insurancePolicyDomainObj);
+        InsurancePolicyEntity savedEntity = insurancePolicyRepository.save(insurancePolicyEntity);
+        return insurancePolicyMapper.toDomain(savedEntity);
+    }
+}
