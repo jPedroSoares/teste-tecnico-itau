1a47f37b3aa9c4c328dc8411bddb205d3c72050d
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 20:37:27 -0300

    feat: add InsurancePolicyMapper for converting between domain and entity objects

---
+++ src/main/java/com/order/api/infrastructure/gateways/InsurancePolicyMapper.java
@@ -0,0 +1,35 @@
+package com.order.api.infrastructure.gateways;
+
+import com.order.api.domain.entity.InsurancePolicy;
+import com.order.api.infrastructure.persistence.InsurancePolicyEntity;
+
+public class InsurancePolicyMapper {
+    InsurancePolicyEntity toEntity(InsurancePolicy insurancePolicyDomainObj) {
+        return new InsurancePolicyEntity(
+                insurancePolicyDomainObj.productId(),
+                insurancePolicyDomainObj.productId(),
+                insurancePolicyDomainObj.category(),
+                insurancePolicyDomainObj.salesChannel(),
+                insurancePolicyDomainObj.paymentMethod(),
+                insurancePolicyDomainObj.totalMonthlyPremiumAmount(),
+                insurancePolicyDomainObj.insuredAmount(),
+                insurancePolicyDomainObj.coverages(),
+                insurancePolicyDomainObj.assistances()
+        );
+    }
+
+    InsurancePolicy toDomain(InsurancePolicyEntity insurancePolicyEntity) {
+        return new InsurancePolicy(
+                insurancePolicyEntity.getCustomerId(),
+                insurancePolicyEntity.getProductId(),
+                insurancePolicyEntity.getCategory(),
+                insurancePolicyEntity.getCoverages(),
+                insurancePolicyEntity.getAssistances(),
+                insurancePolicyEntity.getTotalMonthlyPremiumAmount(),
+                insurancePolicyEntity.getInsuredAmount(),
+                insurancePolicyEntity.getPaymentMethod(),
+                insurancePolicyEntity.getSalesChannel()
+        );
+    }
+
+}
