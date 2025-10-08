a314b9d00a4e98de82ab5eb0f76709d3387a1d9a
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 16:51:28 -0300

    feat: create InsurancePolicy entity

---
+++ src/main/java/com/order/api/domain/entity/InsurancePolicy.java
@@ -0,0 +1,25 @@
+package com.order.api.domain.entity;
+
+import com.order.api.domain.enums.PaymentMethod;
+import com.order.api.domain.enums.PolicyCategory;
+import com.order.api.domain.enums.SalesChannel;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public record InsurancePolicy(
+        UUID customerId,
+        UUID productId,
+        PolicyCategory category,
+        Map<String, Double> coverages,
+        List<String> assistances,
+        Double totalMonthlyPremiumAmount,
+        Double insuredAmount,
+        PaymentMethod paymentMethod,
+        LocalDateTime createdAt,
+        LocalDateTime finishedAt,
+        SalesChannel salesChannel
+) {
+}
