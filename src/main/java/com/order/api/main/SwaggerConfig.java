9306f147eedc524502c413b35fdc21f19416d622
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 23:43:51 -0300

    chore: add Swagger configuration for OpenAPI documentation

---
+++ src/main/java/com/order/api/main/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.order.api.main;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("order-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order API")
+                        .version("1.0")
+                        .description("API for Software Engineering Test"));
+    }
+}
