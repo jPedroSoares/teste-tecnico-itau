c334f5c4d9561419b7e37c2c1e7de23ae186b778
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 23:21:57 -0300

    refactor: move HealthCheckController file

--- src/main/java/com/order/api/infrastructure/web/controllers/HealthCheckController.java
+++ src/main/java/com/order/api/infrastructure/web/controllers/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.order.api.infrastructure.controllers;
+package com.order.api.infrastructure.web.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
