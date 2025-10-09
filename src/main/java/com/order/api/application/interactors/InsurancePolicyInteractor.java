885f1e921bebae3416180ce07609bc4812babb6e
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 23:43:37 -0300

    refactor: simplify InsurancePolicyInteractor to use record for cleaner code

--- src/main/java/com/order/api/application/interactors/InsurancePolicyInteractor.java
+++ src/main/java/com/order/api/application/interactors/InsurancePolicyInteractor.java
@@ -4,11 +4,8 @@ import com.order.api.application.gateways.InsurancePolicyGateway;
 import com.order.api.domain.entity.InsurancePolicy;
 import com.order.api.domain.usecases.CreateInsurancePolicy;
 
-public class InsurancePolicyInteractor implements CreateInsurancePolicy {
-    private final InsurancePolicyGateway insurancePolicyGateway;
-    public InsurancePolicyInteractor(InsurancePolicyGateway insurancePolicyGateway) {
-        this.insurancePolicyGateway = insurancePolicyGateway;
-    }
+public record InsurancePolicyInteractor(
+        InsurancePolicyGateway insurancePolicyGateway) implements CreateInsurancePolicy {
 
     public InsurancePolicy create(InsurancePolicy insurancePolicy) {
         return insurancePolicyGateway.createInsurancePolicy(insurancePolicy);
