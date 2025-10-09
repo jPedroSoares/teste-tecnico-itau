8cf04422c221db0ec6e081a47d2b870b7ecc8f90
jpedrosoares <joao.soares204@hotmail.com>
2025-10-09 20:02:41 -0300

    feat: enhance InsurancePolicyInteractor to include policy validation after creation

--- src/main/java/com/order/api/application/interactors/InsurancePolicyInteractor.java
+++ src/main/java/com/order/api/application/interactors/InsurancePolicyInteractor.java
@@ -1,13 +1,20 @@
 package com.order.api.application.interactors;
 
+import com.order.api.application.dto.PolicyValidationRequest;
 import com.order.api.application.gateways.InsurancePolicyGateway;
+import com.order.api.application.gateways.PolicyValidationGateway;
 import com.order.api.domain.entity.InsurancePolicy;
 import com.order.api.domain.usecases.CreateInsurancePolicy;
 
 public record InsurancePolicyInteractor(
-        InsurancePolicyGateway insurancePolicyGateway) implements CreateInsurancePolicy {
+        InsurancePolicyGateway insurancePolicyGateway,
+        PolicyValidationGateway policyValidationGateway
+) implements CreateInsurancePolicy {
 
     public InsurancePolicy create(InsurancePolicy insurancePolicy) {
-        return insurancePolicyGateway.createInsurancePolicy(insurancePolicy);
+        InsurancePolicy createdInsurancePolicy = insurancePolicyGateway.createInsurancePolicy(insurancePolicy);
+        PolicyValidationRequest policyValidationRequest = new PolicyValidationRequest(createdInsurancePolicy.getId(), createdInsurancePolicy.getCustomerId());
+        policyValidationGateway.validate(policyValidationRequest);
+        return createdInsurancePolicy;
     }
 }
