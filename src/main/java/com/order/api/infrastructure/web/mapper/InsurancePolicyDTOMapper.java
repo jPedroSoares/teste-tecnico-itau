71213c437e3397c9596e2db6f1deb0faf07f55dd
jpedrosoares <joao.soares204@hotmail.com>
2025-10-08 23:22:21 -0300

    feat: add InsurancePolicyDTOMapper for mapping between DTOs and entity

---
+++ src/main/java/com/order/api/infrastructure/web/mapper/InsurancePolicyDTOMapper.java
@@ -0,0 +1,41 @@
+package com.order.api.infrastructure.web.mapper;
+
+import com.order.api.domain.entity.InsurancePolicy;
+import com.order.api.infrastructure.web.dto.CreateInsurancePolicyRequest;
+import com.order.api.infrastructure.web.dto.CreateInsurancePolicyResponse;
+
+import java.util.Optional;
+
+public class InsurancePolicyDTOMapper {
+    public CreateInsurancePolicyResponse toResponse (InsurancePolicy insurancePolicy) {
+        return new CreateInsurancePolicyResponse(
+                insurancePolicy.getId(),
+                insurancePolicy.getCustomerId(),
+                insurancePolicy.getProductId(),
+                insurancePolicy.getCategory(),
+                insurancePolicy.getSalesChannel(),
+                insurancePolicy.getPaymentMethod(),
+                insurancePolicy.getStatus(),
+                insurancePolicy.getCreatedAt(),
+                insurancePolicy.getFinishedAt(),
+                insurancePolicy.getTotalMonthlyPremiumAmount(),
+                insurancePolicy.getInsuredAmount(),
+                insurancePolicy.getCoverages(),
+                insurancePolicy.getAssistances()
+        );
+    }
+
+     public InsurancePolicy toInsurancePolicy (CreateInsurancePolicyRequest request){
+        return new InsurancePolicy(
+                request.customerId(),
+                request.productId(),
+                request.category(),
+                request.coverages(),
+                request.assistances(),
+                request.totalMonthlyPremiumAmount(),
+                request.insuredAmount(),
+                request.paymentMethod(),
+                request.salesChannel()
+        );
+     }
+}
