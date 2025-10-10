418f570fe4aabfac80956be45b6c3f4d4251f39d
jpedrosoares <joao.soares204@hotmail.com>
2025-10-10 02:23:20 -0300

    feat: enhance InsurancePolicyInteractor to include validation logic for created policies

--- src/main/java/com/order/api/application/interactors/InsurancePolicyInteractor.java
+++ src/main/java/com/order/api/application/interactors/InsurancePolicyInteractor.java
@@ -1,20 +1,24 @@
 package com.order.api.application.interactors;
 
 import com.order.api.application.dto.PolicyValidationRequest;
+import com.order.api.application.dto.PolicyValidationResponse;
 import com.order.api.application.gateways.InsurancePolicyGateway;
 import com.order.api.application.gateways.PolicyValidationGateway;
 import com.order.api.domain.entity.InsurancePolicy;
 import com.order.api.domain.usecases.CreateInsurancePolicy;
+import com.order.api.domain.usecases.ValidateInsurancePolicy;
 
 public record InsurancePolicyInteractor(
         InsurancePolicyGateway insurancePolicyGateway,
-        PolicyValidationGateway policyValidationGateway
+        PolicyValidationGateway policyValidationGateway,
+        ValidateInsurancePolicy validateInsurancePolicy
 ) implements CreateInsurancePolicy {
 
     public InsurancePolicy create(InsurancePolicy insurancePolicy) {
         InsurancePolicy createdInsurancePolicy = insurancePolicyGateway.createInsurancePolicy(insurancePolicy);
         PolicyValidationRequest policyValidationRequest = new PolicyValidationRequest(createdInsurancePolicy.getId(), createdInsurancePolicy.getCustomerId());
-        policyValidationGateway.validate(policyValidationRequest);
+        PolicyValidationResponse policyValidationResponse = policyValidationGateway.validate(policyValidationRequest);
+        validateInsurancePolicy.validate(insurancePolicy, policyValidationResponse.classification());
         return createdInsurancePolicy;
     }
 }
