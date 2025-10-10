6aecba889303bd9fda5c8288225088f0b4ed3814
jpedrosoares <joao.soares204@hotmail.com>
2025-10-10 01:29:49 -0300

    feat: add PreferentialCustomerValidationStrategy for policy validation

---
+++ src/main/java/com/order/api/application/interactors/PreferentialCustomerValidationStrategy.java
@@ -0,0 +1,20 @@
+package com.order.api.application.interactors;
+
+import com.order.api.domain.enums.PolicyCategory;
+import com.order.api.domain.usecases.PolicyValidationStrategy;
+
+import java.math.BigDecimal;
+
+public class PreferentialCustomerValidationStrategy implements PolicyValidationStrategy {
+    private final BigDecimal LIFE_LIMIT = BigDecimal.valueOf(800000);
+    private static final BigDecimal AUTO_PROPERTY_LIMIT = BigDecimal.valueOf(450000);
+    private static final BigDecimal OTHER_LIMIT = BigDecimal.valueOf(375000);
+
+    public boolean isValid(BigDecimal insuredAmount, PolicyCategory category) {
+        return switch (category) {
+            case LIFE -> insuredAmount.compareTo(LIFE_LIMIT) <= 0;
+            case AUTO, PROPERTY -> insuredAmount.compareTo(AUTO_PROPERTY_LIMIT) <= 0;
+            default -> insuredAmount.compareTo(OTHER_LIMIT) <= 0;
+        };
+    }
+}
