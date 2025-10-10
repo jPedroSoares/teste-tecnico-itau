aaad161e48cd760c8b0640843ff51ce5f88fe9e2
jpedrosoares <joao.soares204@hotmail.com>
2025-10-10 01:25:52 -0300

    feat: implement HighRiskCustomerValidationStrategy for policy validation

---
+++ src/main/java/com/order/api/application/interactors/HighRiskCustomerValidationStrategy.java
@@ -0,0 +1,20 @@
+package com.order.api.application.interactors;
+
+import com.order.api.domain.enums.PolicyCategory;
+import com.order.api.domain.usecases.PolicyValidationStrategy;
+
+import java.math.BigDecimal;
+
+public class HighRiskCustomerValidationStrategy implements PolicyValidationStrategy {
+    private static final BigDecimal AUTO_LIMIT = BigDecimal.valueOf(250000);
+    private static final BigDecimal PROPERTY_LIMIT = BigDecimal.valueOf(150000);
+    private static final BigDecimal OTHER_LIMIT = BigDecimal.valueOf(125000);
+
+    public boolean isValid(BigDecimal insuredAmount, PolicyCategory category) {
+        return switch (category) {
+            case AUTO -> insuredAmount.compareTo(AUTO_LIMIT) <= 0;
+            case PROPERTY -> insuredAmount.compareTo(PROPERTY_LIMIT) <= 0;
+            default -> insuredAmount.compareTo(OTHER_LIMIT) <= 0;
+        };
+    }
+}
