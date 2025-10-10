25fe4e05b264deb20c397af6c8a308dc74781858
jpedrosoares <joao.soares204@hotmail.com>
2025-10-10 01:21:30 -0300

    feat: implement RegularCustomerValidationStrategy for policy validation

---
+++ src/main/java/com/order/api/application/interactors/RegularCustomerValidationStrategy.java
@@ -0,0 +1,20 @@
+package com.order.api.application.interactors;
+
+import com.order.api.domain.enums.PolicyCategory;
+import com.order.api.domain.usecases.PolicyValidationStrategy;
+
+import java.math.BigDecimal;
+
+public class RegularCustomerValidationStrategy implements PolicyValidationStrategy {
+    private final BigDecimal LIFE_PROPERTY_LIMIT = BigDecimal.valueOf(500000);
+    private static final BigDecimal AUTO_LIMIT = BigDecimal.valueOf(350000);
+    private static final BigDecimal OTHER_LIMIT = BigDecimal.valueOf(255000);
+
+    public boolean isValid(BigDecimal insuredAmount, PolicyCategory category) {
+        return switch (category) {
+            case LIFE, PROPERTY -> insuredAmount.compareTo(LIFE_PROPERTY_LIMIT) <= 0;
+            case AUTO -> insuredAmount.compareTo(AUTO_LIMIT) <= 0;
+            default -> insuredAmount.compareTo(OTHER_LIMIT) <= 0;
+        };
+    }
+}
