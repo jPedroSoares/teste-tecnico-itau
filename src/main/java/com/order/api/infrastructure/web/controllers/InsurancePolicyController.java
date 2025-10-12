fb9b93399278a3d9d1712d48e6a5dd9baa00eaa9
jpedrosoares <joao.soares204@hotmail.com>
2025-10-12 20:28:58 -0300

    feat: add retrieval functionality for insurance policies by ID in InsurancePolicyController

--- src/main/java/com/order/api/infrastructure/web/controllers/InsurancePolicyController.java
+++ src/main/java/com/order/api/infrastructure/web/controllers/InsurancePolicyController.java
@@ -1,23 +1,29 @@
 package com.order.api.infrastructure.web.controllers;
 
+import com.order.api.domain.entity.InsurancePolicy;
 import com.order.api.domain.usecases.CreateInsurancePolicy;
+import com.order.api.domain.usecases.FindInsurancePolicy;
 import com.order.api.infrastructure.web.dto.CreateInsurancePolicyRequest;
 import com.order.api.infrastructure.web.dto.CreateInsurancePolicyResponse;
 import com.order.api.infrastructure.web.mapper.InsurancePolicyDTOMapper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/insurance-policies")
 public class InsurancePolicyController {
     private final CreateInsurancePolicy createInsurancePolicy;
     private final InsurancePolicyDTOMapper insurancePolicyDTOMapper;
+    private final FindInsurancePolicy findInsurancePolicy;
 
-    public InsurancePolicyController(CreateInsurancePolicy createInsurancePolicy, InsurancePolicyDTOMapper insurancePolicyDTOMapper) {
+    public InsurancePolicyController(CreateInsurancePolicy createInsurancePolicy, InsurancePolicyDTOMapper insurancePolicyDTOMapper, FindInsurancePolicy findInsurancePolicy) {
         this.createInsurancePolicy = createInsurancePolicy;
         this.insurancePolicyDTOMapper = insurancePolicyDTOMapper;
+        this.findInsurancePolicy = findInsurancePolicy;
     }
 
     @PostMapping
@@ -25,5 +31,18 @@ public class InsurancePolicyController {
         var insurancePolicy = insurancePolicyDTOMapper.toInsurancePolicy(request);
         var createdInsurancePolicy = createInsurancePolicy.create(insurancePolicy);
         return insurancePolicyDTOMapper.toResponse(createdInsurancePolicy);
+    }
+
+    @GetMapping("/{policyId}")
+    public CreateInsurancePolicyResponse getByPolicyId(
+            @PathVariable UUID policyId) {
+        InsurancePolicy response = findInsurancePolicy.find(policyId);
+        if (response == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Policy not found with id: " + policyId
+            );
+        }
+        return insurancePolicyDTOMapper.toResponse(response);
     }
 }
