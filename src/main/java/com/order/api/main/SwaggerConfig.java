f0bc602323b32e5c21f360a9194508d4b4d408be
jpedrosoares <joao.soares204@hotmail.com>
2025-10-15 20:33:33 -0300

    chore: disable Swagger configuration annotation

--- src/main/java/com/order/api/main/SwaggerConfig.java
+++ src/main/java/com/order/api/main/SwaggerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
